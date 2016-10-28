da83999fce5227da2c50266af62071cbe6a46087
tanlifei <tanlifei@zhixueyun.com>
2016-10-28 15:39:12 +0800

    添加Uitls

--- app/src/androidTest/java/com/tlf/basic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tlf/basic/ExampleInstrumentedTest.java
@@ -1,28 +1,17 @@
 package com.tlf.basic;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 
 /**
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.tlf.basic", appContext.getPackageName());
         String one ="./gradlew clean build bintrayUpload -PbintrayUser=vic-tan -PbintrayKey=8970dbd317d720d9b1176877f90c9bd6a431e650 -PdryRun=false";
-        String tow ="gradlew clean build bintrayUpload -PbintrayUser=vic-tan -PbintrayKey=8970dbd317d720d9b1176877f90c9bd6a431e650 -PdryRun=false";
     }
 }
